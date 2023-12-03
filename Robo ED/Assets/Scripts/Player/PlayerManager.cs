5cc8eb44769f6fdc2b85bac2531fedcbfd5f5da2
Ast <vitor.orsin@estudante.ufscar.br>
2023-12-03 11:02:04 -0300

    Checkpoints and hazards implemented

--- Robo ED/Assets/Scripts/Player/PlayerManager.cs
+++ Robo ED/Assets/Scripts/Player/PlayerManager.cs
@@ -14,22 +14,22 @@ public class PlayerManager : MonoBehaviour
             Respawn();
     }
 
-    private void Respawn()
+    public void Respawn()
     {
         transform.position = respawnPosition.position;
     }
     
-    private void UpdateRespawnPosition(Transform newRespawnPosition)
+    public void UpdateRespawnPosition(Transform newRespawnPosition)
     {
         respawnPosition.transform.position = newRespawnPosition.position;
     }
     
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("UpdateRespawn"))
-        {
-            UpdateRespawnPosition(this.transform);
-            Destroy(other.gameObject);
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("UpdateRespawn"))
+    //     {
+    //         UpdateRespawnPosition(this.transform);
+    //         Destroy(other.gameObject);
+    //     }
+    // }
 }
